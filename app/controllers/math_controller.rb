96dea41899b50129d0b87d7ee2c292539e346474
gallagherb609 <gallagherb609@gmail.com>
2022-08-15 01:03:21 +0000

    math functioning with labels

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,15 +1,57 @@
 class MathController < ApplicationController
   def add_results
     @first = params.fetch("first_num").to_f
-    
+
     @second = params.fetch("second_num").to_f
 
-    @result = @first + @second 
+    @result = @first + @second
 
     render({ :template => "math_templates/add_results.html.erb" })
   end
 
   def add_form
-    render({ :template => "math_templates/add_from.html.erb"})
+    render({ :template => "math_templates/add_form.html.erb" })
+  end
+
+  def subtract_results
+    @first = params.fetch("first_num").to_f
+
+    @second = params.fetch("second_num").to_f
+
+    @result = @first - @second
+
+    render({ :template => "math_templates/subtract_results.html.erb" })
+  end
+
+  def subtract_form
+    render({ :template => "math_templates/subtract_form.html.erb" })
+  end
+
+  def multiplication_results
+    @first = params.fetch("first_num").to_f
+
+    @second = params.fetch("second_num").to_f
+
+    @result = @first * @second
+
+    render({ :template => "math_templates/multiplication_results.html.erb" })
+  end
+
+  def multiplication_form
+    render({ :template => "math_templates/multiplication_form.html.erb" })
+  end
+
+  def division_results
+    @first = params.fetch("first_num").to_f
+
+    @second = params.fetch("second_num").to_f
+
+    @result = @first / @second
+
+    render({ :template => "math_templates/division_results.html.erb" })
+  end
+
+  def division_form
+    render({ :template => "math_templates/division_form.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get("/wizard_add", { :controller => "math", :action => "add_results" })
 
-  get("/wizard_add", { :controller => "math", :action => "add_results"})
+  get("/add", { :controller => "math", :action => "add_form" })
 
-  get("/add", { :controller => "math", :action => "add_form"})
+  get("/subtract", { :controller => "math", :action => "subtract_form" })
+
+  get("/wizard_subtract", { :controller => "math", :action => "subtract_results" })
+
+  get("/multiply", { :controller => "math", :action => "multiplication_form" })
+
+  get("/wizard_multiply", { :controller => "math", :action => "multiplication_results" })
+
+  get("/divide", { :controller => "math", :action => "division_form" })
+
+  get("/wizard_divide", { :controller => "math", :action => "division_results" })
 end
